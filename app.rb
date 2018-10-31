5298ff01f4b09aae68ff210d1084bb322acde859
Alexander Carlin <ahcarlin@gmail.com>
2018-10-31 04:43:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -34,6 +34,7 @@ class App < Sinatra::Base
     @op = params[:operation]
     @num1 = params[:number1].to_i 
     @num2 = params[:number2].to_i 
+    if @op 
     "#{@num1}" + "#{@op}" + "#{@num2}"
   end
   
